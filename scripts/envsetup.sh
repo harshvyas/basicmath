3b9c068c481d6ccb77b4df5298b11c6586bacf9f
Harsh Vyas <harsh.vyas@gmail.com>
2015-03-12 20:39:06 +0000

    fixes

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install build-essential
+sudo apt-get install unzip
 unzip gtest-1.7.0.zip
 cd gtest-1.7.0
 sudo mkdir /opt/gtest /opt/gtest/include /opt/gtest/lib
@@ -8,7 +9,7 @@ sudo mkdir /opt/gtest /opt/gtest/include /opt/gtest/lib
 make
 sudo cp -a include/gtest/ /opt/gtest/include
 sudo cp -a lib/.libs/* /opt/gtest/lib
-rm /opt/gtest/lib/libgtest.la
-rm /opt/gtest/lib/libgtest_main.la
+sudo rm /opt/gtest/lib/libgtest.la
+sudo rm /opt/gtest/lib/libgtest_main.la
 sudo cp -a lib/libgtest.la /opt/gtest/lib
 sudo cp -a lib/libgtest_main.la /opt/gtest/lib
